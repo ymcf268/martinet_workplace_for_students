45f06b26cf112ecbcf1d748fbe4672a1797290b4
ymcf268 <ymcf268@gmail.com>
2026-01-21 20:43:10 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -9,14 +8,13 @@ const firebaseConfig = {
   projectId: "martinet-workplace",
   storageBucket: "martinet-workplace.firebasestorage.app",
   messagingSenderId: "532987474234",
-  appId: "1:532987474234:web:bb13d151c1583762212003",
+  appId: "1:532987474234:web:bb13d151c1583762212003"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Auto anonymous login
 export async function ensureAuth() {
   if (!auth.currentUser) {
     await signInAnonymously(auth);
